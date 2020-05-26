3c82d3614f8f5a57ac6c5c66321ad15f6beb64b7
minefarmer <pgoldfarm@yahoo.com>
2020-05-25 20:25:32 -0500

    Done for the day.

--- py/Oldclass.py
+++ py/Oldclass.py
@@ -47,6 +47,7 @@ while True:
     print('Please choose')
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
+    print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
     if user_choice == '1':
@@ -54,6 +55,9 @@ while True:
         add_transaction(tx_amount, get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain_elements()
+    elif user_choice == 'h':
+        if len(blockchain) >= 1:
+        	blockchain[0] = [2]
     elif user_choice == 'q':
         break
     else:
