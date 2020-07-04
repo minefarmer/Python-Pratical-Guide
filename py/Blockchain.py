2a3b18883c17d9c07c8929bb4e1bb2f2b23a5cf6
C. Richard Matson <pgoldfarm@yahoo.com>
2020-07-03 22:39:21 -0500

    done for the day

--- py/Blockchain.py
+++ py/Blockchain.py
@@ -1,33 +1,34 @@
-# # Initializing our (empty) blockchain list
-blockchain = []
+# Initializing our (empty) blockchain list
+blockchain = [1]
 
 
 def get_last_blockchain_value():
-    """ Returns the last value of the current blockchain. """
+    """ Returns the last value of the current blockchain."""
     return blockchain[-1]
 
 # This function accepts two arguments.
 # One required one (transaction_amount) and one optional one (last_transaction)
 # The optional one is optional because it has a default value => [1]
 def add_value(transaction_amount, last_transaction=[1]):
-    """ Append a new value as well as the last blockchain value to the blockchain.
-
+    """ Appendd a new value as well as the last blockchain value to the blockchain.
+    
     Arguments:
         :transaction_amount: The amount that should be added.
-        :last_transaction: The last blockchain transaction (default [1]).
+        :last_transaction: The amount that should be added.
     """
     blockchain.append([last_transaction, transaction_amount])
-
+    
 
 def get_user_input():
-    """ Returns the input of the user (a new transaction amount) as a float. """
+    """ Returns the input of the user (a new transaction amount) as a floor."""
+    # Get the user input, transform it from a string to a float and store it.
     user_input = float(input('Your transaction amount please: '))
+    return user_input
 
 
 
 
-
-# Get the first transaction input and add the value to the blockchain
+#  Get first transaction input and add the value to the blockchain.
 tx_amount = get_user_input()
 add_value(tx_amount)
 
@@ -35,9 +36,9 @@ while True:
     tx_amount = get_user_input()
     add_value(tx_amount, get_last_blockchain_value())
 
-    # Output the blockchain list to the console
+    # Output for the blockchain list in the console
     for block in blockchain:
-        print('Outputting Block')
+        print('Outputting block')
         print(block)
-
+    
 print('Done!')
