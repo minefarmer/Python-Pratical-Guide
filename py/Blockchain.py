ce2f20d08baa577e12199046e87e683aed8aaf6a
C. Richard Matson <pgoldfarm@yahoo.com>
2020-07-06 11:10:07 -0500

    switching to LMDE

--- py/Blockchain.py
+++ py/Blockchain.py
@@ -1,5 +1,10 @@
 # Initializing our (empty) blockchain list
-blockchain = []
+genesis_block = {
+        'previous_hash': '',
+        'index': 0,
+        'transactions': []
+    }
+blockchain = [genesis_block]
 open_transactions = []
 owner = 'Rich'
 
@@ -26,13 +31,25 @@ def add_transaction(sender, recipient, amount):
     transaction = {
         'sender': sender, 
         'recipient': recipient, 
-        'amount': amount}
-    open_transactions.append()
-
+        'amount': amount
+    }
+    open_transactions.append(transaction)
 
 
 def mine_block():
-    pass
+    last_block = blockchain[-1]
+    last_block['']
+    for keys in last_block:
+        value = last_block[keys]
+        hashed_block = hashed_block + str(value)
+        
+    print(hashed_block)
+    block = {
+        'previous_hash': 'XYZ',
+        'index': len(blockchain),
+        'transactions': open_transactions
+    }
+    blockchain.append(block)
 
 
 def get_transaction_value():
@@ -93,15 +110,20 @@ waiting_for_input = True
 while waiting_for_input:
     print('Please choose')
     print('1: Add a new transaction value')
-    print('2: Output the blockchain blocks')
+    print('2: Mine a new block')
+    print('3: Output the blockchain blocks')
     print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
     if user_choice == '1':
         tx_data = get_transaction_value()
+        recipient, amount = tx_data
         #  add the transaction amountnto the blockchain
-        add_transaction(tx_amount, get_last_blockchain_value())
+        add_transaction(sender=sender, recipient=recipient, amount=amount)
+        print(open_transactions)
     elif user_choice == '2':
+        mine_block()
+    elif user_choice == '3':
         print_blockchain_elements()
     elif user_choice == 'h':
         #  make sure that you don't try to "hack" the blockchain if it's empty
