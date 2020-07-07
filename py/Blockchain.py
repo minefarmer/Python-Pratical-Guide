87c99461346c245b3eff03f5bd64b8bdfc372652
C. Richard Matson <pgoldfarm@yahoo.com>
2020-07-07 12:48:07 -0500

    py/Blockchain: switching to lmde

--- py/Blockchain.py
+++ py/Blockchain.py
@@ -7,10 +7,16 @@ genesis_block = {
 blockchain = [genesis_block]
 open_transactions = []
 owner = 'Carl'
+participants = {'Carl'}
 
 
 def hash_block(block):
-    return '-'.join([str(hash_block[key]) for key in hash_block])
+    return '-'.join([str(hash_block[key]) for key in block])
+
+
+def get_balance(participant):
+    tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participants] for block in blockchain]
+    return tx_sender
 
 
 def get_last_blockchain_value():
@@ -38,17 +44,20 @@ def add_transaction(recipient, sender=owner, amount=1.0):
         'amount': amount
     }
     open_transactions.append(transaction)
-
+    participants.add(sender)
+    participants.add(recipient)
+    
 
 def mine_block():
     last_block = blockchain[-1]
-    hashed_block = '-'.join([str(hash_block[key]) for key in last_block])
+    hashed_block = hash_block(last_block)
     block = {
         'previous_hash': hashed_block,
         'index': len(blockchain),
         'transactions': open_transactions
     }
     blockchain.append(block)
+    return True
 
 
 def get_transaction_value():
@@ -94,6 +103,7 @@ while waiting_for_input:
     print('1: Add a new transaction value ')
     print('2: Mine a new block')
     print('3: Output the blockchain blocks')
+    print('4: Output participants')
     print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
@@ -104,9 +114,12 @@ while waiting_for_input:
         add_transaction(recipient, amount=amount)
         print(open_transactions)
     elif user_choice == '2':
-        mine_block()
+        if mine_block():
+            open_transactions = []
     elif user_choice == '3':
         print_blockchain_elements()
+    elif user_choice == '4':
+        print(participants)
     elif user_choice == 'h':
         # Make sure that you don't try to "hack" the blockchain if it's empty
         if len(blockchain) >= 1:
@@ -125,6 +138,7 @@ while waiting_for_input:
         print('Invalid blockchain!')
         # Break out of the loop
         break
+    print(get_balance('Carl'))
 else:
     print('User left!')
 
